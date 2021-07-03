93a68bf682b263f45c1943dff840a6b4d9f1c224
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-07-03 21:58:25 +0800

    Got modal working for now

---
+++ scripts/aboutUs.js
@@ -0,0 +1,64 @@
+fetch("../data/members.json")
+  .then((res) => res.json())
+  .then((data) => {
+    let output = "";
+
+    data.members.forEach((member) => {
+      output += `
+        <div class="member" id="member-${member.id}">
+          <img src="./images/hero.jpg" alt="member">
+          <h1 class="member-name">${member.name}</h1>
+          <p class="member-position">${member.role}</p>
+        </div>
+      `;
+    });
+
+    document.getElementById("members").innerHTML = output;
+
+    const membersEl = document.getElementsByClassName("member");
+
+    for (let i = 0; i < membersEl.length; i++) {
+      const memberEl = membersEl[i];
+      const memberId = parseInt(memberEl.id.split("-")[1]);
+
+      memberEl.addEventListener("click", async () => {
+        const data = await fetch("../data/members.json");
+        const memberData = (await data.json()).members[memberId - 1];
+
+        const modal = document.getElementById("member-modal");
+
+        modal.innerHTML = `
+          <div class="member-modal-overlay loading" id="member-modal-overlay"></div>
+          <div class="member-modal-content loading">
+            <img
+              class="member-modal-content-img"
+              src="./images/hero.jpg"
+              alt="hero"
+            />
+            <div class="member-modal-content-text">
+              <h1 class="member-modal-name">${memberData.name}</h1>
+              <p class="member-modal-role">${memberData.role}</p>
+              <p class="member-modal-description">${memberData.description}</p>
+            </div>
+          </div>
+        `;
+
+        setTimeout(() => {
+          modal
+            .querySelector(".member-modal-overlay")
+            .classList.remove("loading");
+
+          modal
+            .querySelector(".member-modal-content")
+            .classList.remove("loading");
+        }, 10);
+
+        document
+          .getElementById("member-modal-overlay")
+          .addEventListener("click", () => {
+            modal.innerHTML = "";
+          });
+      });
+    }
+  })
+  .catch((err) => console.log(err));
